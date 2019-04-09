71c3c98274667277d45525dad06b7d080384c12e
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2019-04-09 10:24:12 -0400

    changed sh and invoke

--- bin/nh-cc3d-vascular-tumor.sh
+++ bin/nh-cc3d-vascular-tumor.sh
@@ -19,11 +19,11 @@ export COMPUCELL3D_STEPPABLE_PATH=${PREFIX_CC3D}/lib/CompuCell3DSteppables
 export LD_LIBRARY_PATH=${COMPUCELL3D_STEPPABLE_PATH}:${COMPUCELL3D_PLUGIN_PATH}:${PREFIX_CC3D}/lib/python:${PREFIX_CC3D}/lib:${VTK_LIB_DIR}:${LD_LIBRARY_PATH}
 export SWIG_LIB_INSTALL_DIR=${PREFIX_CC3D}/lib/python
 export PYTHON_MODULE_PATH=${PREFIX_CC3D}/pythonSetupScripts
-
-export DEMO_PATH=${PREFIX_CC3D}/Demos/Models/cellsort/cellsort_2D
+# BookChapterDemos_ComputationalMethodsInCellBiology/VascularTumor
+export DEMO_PATH=${PREFIX_CC3D}/BookChapterDemos_ComputationalMethodsInCellBiology/VascularTumor
 echo ${DEMO_PATH}
 
-export USER_DEMO_PATH=${HOME}/CompuCell3D_Demos/cellSort2D/
+export USER_DEMO_PATH=${HOME}/CompuCell3D_Demos/VascularTumor/
 echo "Setting user files"
 echo ${USER_DEMO_PATH}
 
@@ -41,7 +41,7 @@ export SOSLIB_PATH=${PREFIX_CC3D}/examplesSoslib
 
 echo "CompuCell3D - version $COMPUCELL3D_MAJOR_VERSION.$COMPUCELL3D_MINOR_VERSION.$COMPUCELL3D_BUILD_VERSION"
 
-${PYTHON_EXEC} ${PREFIX_CC3D}/player5/compucell3d.pyw -i ${USER_DEMO_PATH}/cellsort_2D.cc3d -o ${USER_DEMO_PATH}/output/ -f 50 --currentDir=${PWD}
+${PYTHON_EXEC} ${PREFIX_CC3D}/player5/compucell3d.pyw -i ${USER_DEMO_PATH}/VascularTumor.cc3d -o ${USER_DEMO_PATH}/output/ -f 50 --currentDir=${PWD}
 exit_code=$?
 
 exit ${exit_code}
